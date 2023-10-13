beb2b3f0fd0d936b416bcb0def8adbb7b040ca09
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-10-13 13:26:34 +0900

    bottomNavigationView 등록완료

--- app/src/main/java/com/moneyminions/mvvmtemplate/MainActivity.kt
+++ app/src/main/java/com/moneyminions/mvvmtemplate/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.moneyminions.mvvmtemplate.base.BaseActivity
 import com.moneyminions.mvvmtemplate.databinding.ActivityMainBinding
 import com.moneyminions.mvvmtemplate.di.ApplicationClass.Companion.sharedPreferences
@@ -13,10 +15,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initNavController()
+        setBottomNavigationView()
+    }
+
+    private fun setBottomNavigationView(){
+        bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setupWithNavController(navController)
     }
 
     private fun initNavController() {

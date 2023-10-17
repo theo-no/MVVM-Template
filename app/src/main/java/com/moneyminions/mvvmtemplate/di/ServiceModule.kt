94f1c27b3c45aceca93540360c4dbd6b91cc8baa
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-10-17 13:28:29 +0900

    ✨ feat : 서비스 모듈 생성 | 차선호

--- app/src/main/java/com/moneyminions/mvvmtemplate/di/ServiceModule.kt
+++ app/src/main/java/com/moneyminions/mvvmtemplate/di/ServiceModule.kt
@@ -1,4 +1,65 @@
 package com.moneyminions.mvvmtemplate.di
 
+import android.content.SharedPreferences
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.moneyminions.mvvmtemplate.interceptor.RequestInterceptor
+import com.moneyminions.mvvmtemplate.interceptor.ResponseInterceptor
+import com.moneyminions.mvvmtemplate.service.BusinessService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object ServiceModule {
+    @Singleton
+    @Provides
+    @Named("BASE_URL")
+    fun BaseUrl() : String = "ddd"
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(5000, TimeUnit.MILLISECONDS)
+        .connectTimeout(5000, TimeUnit.MILLISECONDS)
+        .addInterceptor(HttpLoggingInterceptor())
+//        .addInterceptor(ResponseInterceptor(preferenceDataSource))
+//        .addInterceptor(RequestInterceptor(preferenceDataSource))
+//            .addNetworkInterceptor(XAccessTokenInterceptor()) // JWT 자동 헤더 전송
+//            .addInterceptor(AddCookiesInterceptor())  //쿠키 전송
+//            .addInterceptor(ReceivedCookiesInterceptor()) //쿠키 추출
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+//    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        @Named("BASE_URL") baseUrl: String,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                    .create(),
+            ),
+        )
+        .client(provideOkHttpClient())
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideBusinessService(
+        retrofit: Retrofit,
+    ): BusinessService = retrofit.create(BusinessService::class.java)
 }
